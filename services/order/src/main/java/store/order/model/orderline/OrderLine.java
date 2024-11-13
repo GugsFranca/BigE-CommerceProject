3b5cc20a77c19e87115f17719d88bcdc53fccea5
Gustavo de França <gustavonota20@hotmail.com>
2024-11-13 12:53:37 -0300

    Creating the order and order line Models

---
+++ services/order/src/main/java/store/order/model/orderline/OrderLine.java
@@ -0,0 +1,25 @@
+package store.order.model.orderline;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import store.order.model.order.Order;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "customer_order")
+public class OrderLine {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+    private Integer productId; //Product_service
+    private double quantity;
+}
